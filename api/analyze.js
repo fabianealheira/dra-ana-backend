f1a3f4a221ec30cad74322d9c3674e2b10eabc3c
fabianealheira <fabiane.alheira@gmail.com>
2026-02-28 15:59:21 -0300

    Update analyze.js

--- api/analyze.js
+++ api/analyze.js
@@ -17,7 +17,7 @@ export default async function handler(req, res) {
       messages: messages,
     });
 
-    return res.status(200).json({
+  return res.status(200).json({
   answer: response.choices[0].message.content,
 });
 
